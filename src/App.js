4b7d365fd2ef2f673530cead27e799ce46fcfdc1
Ceabta <leocesario.abta@gmail.com>
2022-08-15 09:39:32 -0300

    Aula 6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoas from './components/Pessoas';
+import Frase from './components/Frase';
 
 function App() {
   const nome = "Maria"
 
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
       <SayMyName nome="Matheus"/>
       <SayMyName nome="João"/>
       <SayMyName nome={nome}/>
