7877c144cb9a44779ae874a1ad31ccb5d219b84c
Ceabta <leocesario.abta@gmail.com>
2022-08-16 09:52:34 -0300

    Aula 12 - If

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Frase from './components/Frase';
 import List from './components/List';
 import Evento from './components/Eventos';
 import Form from './components/Form';
+import Condicional from './components/Condicional';
 
 function App() {
   const nome = "Maria"
@@ -23,10 +24,18 @@ function App() {
         profissao="Programador" 
         foto="https://via.placeholder.com/150"
       />
+      <hr/><hr/>
+      {/* ----------------------------------------------------- */}
       <List />
+      <hr/><hr/>
+      {/* ----------------------------------------------------- */}
       <h1>Testando Eventos</h1>
       <Evento numero="1"/>
       <Form /> 
+      <hr/><hr/>
+      {/* ----------------------------------------------------- */}
+      <h1>Renderização Condicional</h1>
+      <Condicional />
     </div>
   )
 }
